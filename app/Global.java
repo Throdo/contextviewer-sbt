1de8e83b830c5783c3cd1f4aa277c4599fc16df7
throdo <herve.darritchon@gmail.com>
2013-06-02 16:13:09 +0200

    Ajout de la Classe Global pour gérer :  - le chargement de l'application  - le déchargement de l'application  - la gestions des pages d'erreur par défaut :   - HTTP 500   - HTTP 404   - Action not found

---
+++ app/Global.java
@@ -0,0 +1,49 @@
+import play.Application;
+import play.GlobalSettings;
+import play.Logger;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.mvc.Results;
+
+import static play.mvc.Results.internalServerError;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: throdo
+ * Date: 02/06/13
+ * Time: 15:01
+ */
+public class Global extends GlobalSettings {
+
+    @Override
+    public void onStart(Application app) {
+        Logger.info("Application has started");
+    }
+
+    @Override
+    public void onStop(Application app) {
+        Logger.info("Application shutdown...");
+    }
+
+    @Override
+    public Result onError(Http.RequestHeader request, Throwable t) {
+        return internalServerError(
+                views.html.errorPage.render(t)
+        );
+    }
+
+    @Override
+    public Result onBadRequest(Http.RequestHeader request, String error) {
+        return Results.badRequest("Don't try to hack the URI!");
+    }
+
+    @Override
+    public Result onHandlerNotFound(Http.RequestHeader request) {
+        return Results.notFound(
+                views.html.pageNotfound.render(request.uri())
+        );
+    }
+
+}
+
