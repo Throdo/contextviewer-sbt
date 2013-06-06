9848673f6dc6039bb656b95032efe3cb541a1da3
throdo <herve.darritchon@gmail.com>
2013-06-06 21:20:18 +0200

    Ajout de Javadoc dans la Classe Global

--- app/Global.java
+++ app/Global.java
@@ -19,18 +19,36 @@ import static play.mvc.Results.internalServerError;
  */
 public class Global extends GlobalSettings {
 
+    /**
+     * Méthode qui est lancé au démarrage de l'application (premier appel)
+     *
+     * @param app
+     */
     @Override
     public void onStart(Application app) {
         Logger.info("Application has started");
         OrangeClusterManagerHandler.getinstance();
     }
 
+    /**
+     * Méthode qui est lancé à la fermeture de l'application.
+     *
+     * @param app
+     */
     @Override
     public void onStop(Application app) {
         Logger.info("Application shutdown...");
         OrangeClusterManagerHandler.releaseInstance();
     }
 
+    /**
+     * Méthode appelé dans le cas d'une erreur lors du traitement. Code d'erreur HTTP 500.
+     * Cette méthode intercepte toute les exceptions et évite d'afficher la stack Java.
+     *
+     * @param request
+     * @param t
+     * @return
+     */
     @Override
     public Result onError(Http.RequestHeader request, Throwable t) {
         return internalServerError(
@@ -38,11 +56,24 @@ public class Global extends GlobalSettings {
         );
     }
 
+    /**
+     * Méthode qui permet d'afficher une page d'erreur spéciale lors d'une saisie d'une action qui n'existe pas dans les routes.
+     *
+     * @param request
+     * @param error
+     * @return
+     */
     @Override
     public Result onBadRequest(Http.RequestHeader request, String error) {
         return Results.badRequest("Don't try to hack the URI!");
     }
 
+    /**
+     * Méthode qui permet d'afficher une page spéciale lors d'une erreur HTTP 404.
+     *
+     * @param request
+     * @return
+     */
     @Override
     public Result onHandlerNotFound(Http.RequestHeader request) {
         return Results.notFound(
